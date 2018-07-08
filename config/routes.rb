6b37f4f57140d64f974361e86c4b15abe0617bf8
Chris Powell <chris.ian.powell@gmail.com>
2018-07-08 11:16:42 -0600

    updated comment feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :categories
+  resources :jobs do
+    resources :comment
+  end
   resources :companies do
     resources :jobs
   end
--- spec/features/comments/user_can_see_comments_spec.rb
+++ spec/features/comments/user_can_see_comments_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 describe 'User sees all comments for a single job' do
-  describe 'a user can see all comments for a job' do
+  scenario 'a user can see all comments for a job' do
     category = Category.create!(title: 'Web Development')
     company = Company.create!(name: "ESPN")
-    comment_1 = Comment.create!(tbd)
-    comment_2 = Comment.create!(tbd)
     job = Job.create!(title: "Developer", level_of_interest: 90, city: "Denver", company_id: company.id, category_id: category.id)
+    comment_1 = Comment.create!(content: 'tbd', job_id: job.id)
+    comment_2 = Comment.create!(content: 'tbddfr00000', job_id: job.id)
 
-    visit company_job_path(company.id, job.id)
+    visit company_job_path(company, job)
 
-    expect(page).to have_content(comment_1)
-    expect(page).to have_content(comment_2)
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{job.id}")
+    expect(page).to have_content(comment_1.content)
+    expect(page).to have_content(comment_2.content)
   end
 end
