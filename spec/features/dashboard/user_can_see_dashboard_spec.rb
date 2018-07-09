2b1286d225fd9ca64a7924a1abb18c4238446faf
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-08 18:47:21 -0600

    add feature test for jobs by interest on dashboard

--- spec/features/dashboard/user_can_see_dashboard_spec.rb
+++ spec/features/dashboard/user_can_see_dashboard_spec.rb
@@ -8,4 +8,26 @@ describe "User can visit /dashboard" do
     expect(page).to have_content("Job Tracker")
   end
 
+  scenario "a user can see a count of jobs by level of interest" do
+    category_1 = Category.create!(title: 'Web Development')
+    category_2 = Category.create!(title: 'Education')
+    category_3 = Category.create!(title: 'Sales')
+    category_4 = Category.create!(title: 'Accounting')
+    company_1 = Company.create!(name: "ESPN")
+    company_2 = Company.create!(name: "Turing")
+    company_3 = Company.create!(name: "Macy's")
+    company_4 = Company.create!(name: "Adidas")
+    job_1 = Job.create!(title: "Developer", level_of_interest: 4, city: "Denver", company_id: company_1.id, category_id: category_1.id)
+    job_2 = Job.create!(title: "Teacher", level_of_interest: 5, city: "Boulder", company_id: company_2.id, category_id: category_2.id)
+    job_3 = Job.create!(title: "Sales Rep", level_of_interest: 2, city: "New York", company_id: company_3.id, category_id: category_1.id)
+    job_4 = Job.create!(title: "Accountant", level_of_interest: 5, city: "Seattle", company_id: company_4.id, category_id: category_4.id)
+    visit '/dashboard'
+
+    expect(page).to have_content("Jobs by Interest:")
+    expect(page).to have_content("***** (2)Jobs")
+    expect(page).to have_content("**** (1)Job")
+    expect(page).to have_content("** (1)Job")
+  end
+
+
 end
