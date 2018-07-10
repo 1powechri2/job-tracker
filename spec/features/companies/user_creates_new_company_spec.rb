69a0f30a38f57ba43369dcd8929eb28fce637c0e
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-09 19:10:25 -0600

    update company create feature test

--- spec/features/companies/user_creates_new_company_spec.rb
+++ spec/features/companies/user_creates_new_company_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe "User creates a new company" do
   scenario "a user can create a new company" do
+    company_name = 'ESPN'
     visit new_company_path
 
-    fill_in "company[name]", with: "ESPN"
+    fill_in :company_name, with: company_name
     click_button "Create"
 
-    expect(current_path).to eq("/companies/#{Company.last.id}")
     expect(page).to have_content("ESPN")
     expect(Company.count).to eq(1)
   end
